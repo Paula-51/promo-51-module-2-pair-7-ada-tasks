094f57e45d385906eb2527ccb7e4ec95bfc31599
claudiarelat <clauprelat@gmail.com>
2025-05-08 22:37:49 +0200

    primer javascript

--- js/main.js
+++ js/main.js
@@ -7,13 +7,30 @@ const newInput = document.querySelector(".js-input-find");
 const btnFind = document.querySelector(".js-btn-buscar");
 const tareas = [];
 
+
+function cargarTareasIniciales() {
+  const labels = document.querySelectorAll(".js-check label");
+
+  labels.forEach(label => {
+    const texto = label.textContent.trim(); //innerText también funciona (si ni tiene ningún estilo CSS que lo oculte).
+    tareas.push(texto);
+  });
+}
+
+cargarTareasIniciales(); 
+
 function addTask(event) {
-    let tarea = newTask.value;
+    const tarea = newTask.value.trim();
+    if (!tarea) return; // evita tareas vacías y detiene la función de abajo 
+
+    tareas.push(tarea); // añadimos la tarea al array
+
     lista.innerHTML += `<li>
-                    <input type="checkbox" id="tarea1">
-                    <label for="tarea1"> ${tarea}</label>
-                    </li>`;
-    console.log (tarea);
+        <input type="checkbox" id="${tarea}">
+        <label for="${tarea}">${tarea}</label>
+    </li>`;
+
+    newTask.value = ""; // limpiamos el input
 }
 
 function checkear(event) {
@@ -23,12 +40,38 @@ function checkear(event) {
     }
   }
 
-function buscar (event) {
-    let oldTask = newInput.value;
+function buscar(event) {
+  const textoBuscado = newInput.value.trim().toLowerCase();
 
+  // if (!textoBuscado) {
+  //   return;
+  // }
+  
+  // Filtrar las tareas que contienen el texto buscado
+  const resultados = tareas.filter(tarea =>
+    tarea.toLowerCase().includes(textoBuscado)
+  );
+
+  // Limpiar la lista antes de agregar los resultados filtrados
+  lista.innerHTML = "";  
+
+  // Mostrar las tareas filtradas
+  if (resultados.length > 0) {
+    resultados.forEach(tarea => {
+      lista.innerHTML += `<li>
+        <input type="checkbox" id="${tarea}">
+        <label for="${tarea}">${tarea}</label>
+      </li>`;
+    });
+  } 
+
+  // Limpiar el input de búsqueda
+  newInput.value = "";
 }
+
 
 
 
 btnAdd.addEventListener ("click", addTask);  
 lista.addEventListener("change", checkear);
+btnFind.addEventListener("click", buscar);
